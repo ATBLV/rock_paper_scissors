da89e85be0a92ffcdd21e66ba335187bbc9e9663
ATBLV <andtblv@gmail.com>
2022-01-24 22:18:20 +1100

    The fugly but functional version

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,68 +1,104 @@
 console.log("TITLE: ROCK PAPER SCISSORS!!");
 
-game();
+let playerScore = 0;
+let computerScore = 0;
+let gameEndScore = 5;
 
-function game(){
-    for(let i = 0; i<5; i++){
-        let playerSelection = playerChoice();
-        let computerSelection = computerPlay();
-        console.log(playRound(playerSelection,computerSelection));
+updateScore();
+
+const button = document.querySelectorAll("#rock, #paper, #scissors");
+
+button.forEach(button => button.addEventListener('click',function(){  
+
+    if(playerScore < gameEndScore && computerScore < gameEndScore){
+        let result = playRound(this.id,computerPlay());
+        updateScore();
+        resultOutput('afterbegin',result, 'result');
+        if(playerScore === gameEndScore || computerScore === gameEndScore){
+            playerScore === gameEndScore ? gameEnd('PLAYER'): gameEnd('COMPUTER');
+        }
     }
-    console.log("GAME OVER");
-    alert("GAME OVER")
+}));
+
+function selectionDisplay(player,computer){
+    document.getElementById('playerChoice').innerHTML = '';
+    document.getElementById('playerChoice').insertAdjacentText('afterbegin',player);
+    document.getElementById('computerChoice').innerHTML = '';
+    document.getElementById('computerChoice').insertAdjacentText('afterbegin',computer);
+
+}
+
+function updateScore(){
+    document.getElementById('playerScore').innerHTML = '';
+    document.getElementById('playerScore').insertAdjacentText('afterbegin',playerScore);
+    document.getElementById('computerScore').innerHTML = '';
+    document.getElementById('computerScore').insertAdjacentText('afterbegin',computerScore);
+
+}
+
+function computerPlay(){                                                    
+    const selection = ['rock', 'paper', 'scissors'];
+    return selection[Math.floor(Math.random()*3)];
+}
+
+function resultOutput(position, result, elementId){
+    const inputConvert = {'rock': 'Rock', 'paper': 'Paper', 'scissors': 'Scissors'}
+    document.getElementById(elementId).innerHTML = '';
+    document.getElementById(elementId).insertAdjacentText(position,result);
 }
 
 function playRound(playerSelection, computerSelection) {
-
-    const convInput = {'r': 'Rock', 'p': 'Paper', 's': 'Scissors'}
-
+    const inputConvert = {'rock': 'Rock', 'paper': 'Paper', 'scissors': 'Scissors'}
+    let selections = `Player chose ${inputConvert[playerSelection]} and Computer chose ${inputConvert[computerSelection]}!`;
+    
+    selectionDisplay(`${inputConvert[playerSelection]}`,`${inputConvert[computerSelection]}`); 
+    if(computerSelection === playerSelection){
+        return "Game is a tie!";
+    }
     switch(playerSelection) {
-        case 'r': 
-            console.log(`Player chose ${convInput[playerSelection]} and Computer chose ${convInput[computerSelection]}!`);
-            if(computerSelection === playerSelection){
-                return ("GAME IS A TIE!!");
-            } else if(computerSelection === 'p' ){
-                return ("Paper beats rock. Computer wins!");
-            } else if(computerSelection === 's'){
-                return ("Rock beats scissors! Player wins!");
+        case 'rock': 
+            if(computerSelection === 'paper' ){
+                computerScore ++;
+                return "Paper beats rock. Computer wins!";
+            } else {
+                playerScore++;
+                return "Rock beats scissors! Player wins!";
             }
-            break;
-        case 'p':
-            console.log(`Player chose ${convInput[playerSelection]} and Computer chose ${convInput[computerSelection]}!`);
-            if(computerSelection === playerSelection){
-                return ("GAME IS A TIE!!");
-            } else if(computerSelection === 'r' ){
-                return ("Paper beats rock. Player wins!");
-            } else if(computerSelection === 's'){
-                return ("Scissors beats paper! Computer wins!");
+        case 'paper':
+            if(computerSelection === 'rock' ){
+                playerScore++;
+                return "Paper beats rock. Player wins!";
+            } else {
+                computerScore++;
+                return "Scissors beats paper! Computer wins!";
             }
-            break;
-        case 's':
-            console.log(`Player chose ${convInput[playerSelection]} and Computer chose ${convInput[computerSelection]}!`);
-            if(computerSelection === playerSelection){
-                return ("GAME IS A TIE!!");
-            } else if(computerSelection === 'r' ){
-                return ("Rock beats scissors. Computer wins!");
-            } else if(computerSelection === 'p'){
-                return ("Scissors beats paper! Player wins!");
+        case 'scissors':
+            if(computerSelection === 'rock' ){
+                computerScore++;
+                return "Rock beats scissors. Computer wins!";
+            } else {
+                playerScore++;
+                return "Scissors beats paper! Player wins!";
             }
-            break;
         };
 }
-  
-function playerChoice() {                                                           // takes only the 1st letter regardless of input typed and verifies whether it is an acceptable selection
-    let choice = prompt("Enter rock, paper or scissors!").toLowerCase().charAt(0);
 
-    while(choice != "r" && choice != "p" && choice != "s"){          
-        console.log("Invalid input! Try again");
-        alert("Invalid input! try again.")
-        choice = playerChoice();
-    }
-    return choice;
-}
+function gameEnd(player){
+    let play = `${player} WINS THE GAME!!!!`;
+    updateScore();
+    resultOutput('beforeend',play,'newgame');
 
-function computerPlay(){                                                    // converts and returns a randomly generated number into one of the 3 string choices from 'selection[]'
-    const selection = ['r', 'p', 's'];
-    let randomNumber = Math.floor(Math.random()*3) ;
-    return selection[randomNumber];
+    button.forEach(button => button.disabled = true);
+    let btn = document.createElement("button");
+    btn.innerHTML = "New game?";
+    document.getElementById('newgame').appendChild(btn);
+    btn.classList.add('button', 'btn', 'btn-primary', 'btn-lg');
+    btn.style.marginTop = "1%";
+    btn.id = 'new';
+
+    document.addEventListener('click',function(e){
+        if(e.target && e.target.id== 'new'){
+            location.reload();
+         }
+     });
 }
